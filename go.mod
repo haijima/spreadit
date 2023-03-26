f1d4c932434022c449efd00cc4c16b468dfc749c
haijima <k.haijima@gmail.com>
2023-03-26 22:31:47 +0900

    feat: Show spinner and message

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/haijima/spreadit
 go 1.19
 
 require (
+	github.com/briandowns/spinner v1.23.0
 	github.com/h2non/gock v1.2.0
 	github.com/haijima/cobrax v0.0.4
 	github.com/spf13/afero v1.9.5
@@ -16,6 +17,7 @@ require (
 	cloud.google.com/go/compute v1.18.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
@@ -26,6 +28,8 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -37,6 +41,7 @@ require (
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/oauth2 v0.6.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
--- internal/spreadsheet.go
+++ internal/spreadsheet.go
@@ -35,12 +35,20 @@ func NewSheetsService(ctx context.Context) (*SheetsService, error) {
 	return &SheetsService{srv}, nil
 }
 
-func (s *SheetsService) AddFileDataToNewSheet(ctx context.Context, r io.Reader, opt AddOption) error {
+func (s *SheetsService) RetrieveSpreadsheet(ctx context.Context, spreadsheetId string) (*sheets.Spreadsheet, error) {
+	spreadsheet, err := s.srv.Spreadsheets.Get(spreadsheetId).Context(ctx).Do()
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve data from sheet: %v", err)
+	}
+	return spreadsheet, nil
+}
+
+func (s *SheetsService) ReadCsv(r io.Reader) (*sheets.ValueRange, error) {
 	// Read CSV data
 	reader := csv.NewReader(r)
 	records, err := reader.ReadAll()
 	if err != nil {
-		return fmt.Errorf("unable to read CSV data from file or stdin: %v", err)
+		return nil, fmt.Errorf("unable to read CSV data from file or stdin: %v", err)
 	}
 
 	// Convert CSV data to Google Sheets API value range format
@@ -56,20 +64,23 @@ func (s *SheetsService) AddFileDataToNewSheet(ctx context.Context, r io.Reader, 
 		Values: values,
 	}
 
-	// Create a new sheet
-	if !opt.Append {
-		req := &sheets.BatchUpdateSpreadsheetRequest{
-			Requests: []*sheets.Request{
-				{AddSheet: &sheets.AddSheetRequest{Properties: &sheets.SheetProperties{Title: opt.NewSheetTitle}}},
-			},
-		}
-		if _, err := s.srv.Spreadsheets.BatchUpdate(opt.SpreadsheetID, req).Context(ctx).Do(); err != nil {
-			return fmt.Errorf("unable to add sheet to spreadsheet: %v", err)
-		}
-	}
+	return valueRange, nil
+}
 
-	// Add CSV data to sheet
-	if _, err := s.srv.Spreadsheets.Values.Update(opt.SpreadsheetID, fmt.Sprintf("'%s'!%s", opt.NewSheetTitle, opt.Range), valueRange).ValueInputOption("RAW").Context(ctx).Do(); err != nil {
+func (s *SheetsService) CreateNewSheet(ctx context.Context, spreadSheetId, sheetTitle string) error {
+	req := &sheets.BatchUpdateSpreadsheetRequest{
+		Requests: []*sheets.Request{
+			{AddSheet: &sheets.AddSheetRequest{Properties: &sheets.SheetProperties{Title: sheetTitle}}},
+		},
+	}
+	if _, err := s.srv.Spreadsheets.BatchUpdate(spreadSheetId, req).Context(ctx).Do(); err != nil {
+		return fmt.Errorf("unable to add sheet to spreadsheet: %v", err)
+	}
+	return nil
+}
+
+func (s *SheetsService) AddCsvToSheet(ctx context.Context, spreadSheetId, sheetTitle, a1Range string, valueRange *sheets.ValueRange) error {
+	if _, err := s.srv.Spreadsheets.Values.Update(spreadSheetId, fmt.Sprintf("'%s'!%s", sheetTitle, a1Range), valueRange).ValueInputOption("RAW").Context(ctx).Do(); err != nil {
 		return fmt.Errorf("unable to add csv data to sheet: %v", err)
 	}
 	return nil
